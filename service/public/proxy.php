2c56624b5fa362a22ab730f3ebc99b80c23dddcf
Helppo Elämä <nastradoomus@cs-go.fi>
2021-02-19 11:23:55 +0200

    💌 Some fixes...

--- service/public/proxy.php
+++ service/public/proxy.php
@@ -3,7 +3,6 @@ if ($_GET["start_date"]) {
     $start_date = htmlspecialchars($_GET["start_date"]);
     if ($_GET["end_date"]) {
         $end_date = htmlspecialchars($_GET["end_date"]);
-        header("Access-Control-Allow-Origin: *");
          header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
          header("X-Requested-With: XMLHttpRequest");
         $jsonData = file_get_contents("https://app.thebookingbutton.com/api/v1/properties/alarautaoydirect/rates.json?start_date=".$start_date."&end_date=".$end_date);
